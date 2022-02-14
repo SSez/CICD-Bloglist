93b7d52e4542504e86a2daa3024204abe15c0050
SSez <sez@noreply.github.com>
2022-02-14 21:13:48 +0200

    pipeline

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.tokenExtractor)
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
