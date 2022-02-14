4734b4252eb1b8da780eaa197d0e58a809193179
SSez <sez@noreply.github.com>
2022-02-14 17:33:40 +0200

    tests

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,14 +1,14 @@
 describe('Blog app', function() {
     beforeEach(function() {
-      cy.request('POST', 'http://localhost:3003/api/testing/reset')
-      cy.visit('http://localhost:3003')
+      cy.request('POST', 'http://localhost:3001/api/testing/reset')
+      cy.visit('http://localhost:3001')
       const user = {
         name: 'admin',
         username: 'root',
         password: 'secret'
       }
-      cy.request('POST', 'http://localhost:3003/api/users/', user)
-      cy.visit('http://localhost:3003')
+      cy.request('POST', 'http://localhost:3001/api/users/', user)
+      cy.visit('http://localhost:3001')
     })
 
     it('Login form is shown', function() {
