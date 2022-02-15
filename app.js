e3f82b62e452ace8290410d9553a17219a42c9e3
SSez <sez@noreply.github.com>
2022-02-15 13:03:16 +0200

    Pipeline

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.get('/health', (req, res) => {
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use(express.static('build'))
 
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
