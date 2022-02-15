170b8510e00ab9414b007a43a0f5011170a81878
SSez <sez@noreply.github.com>
2022-02-15 13:27:01 +0200

    Pipeline

--- cypress/integration/blog.spec.js
+++ cypress/integration/blog.spec.js
@@ -1,11 +1,11 @@
 /// <reference types="cypress" />
 describe('Blog app', function () {
   it('Login page can be opened', function () {
-    cy.visit('http://localhost:3003'); // eslint-disable-line
+    cy.visit('http://localhost:3002'); // eslint-disable-line
     cy.contains('Login to application'); // eslint-disable-line
   })
   it('You could try to login', function () {
-    cy.visit('http://localhost:3003'); // eslint-disable-line
+    cy.visit('http://localhost:3002'); // eslint-disable-line
     cy.contains('login').click(); // eslint-disable-line
   })
 })
--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
-server.listen(config.PORT || 3003, () => {
+server.listen(config.PORT || 3002, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
